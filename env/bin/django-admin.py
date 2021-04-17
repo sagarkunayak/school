9b9915965189c7089c143961bb0e8642d4fa7a59
Sagar Kumar Nayak <sagarkumarnayak@Sagars-MacBook-Air.local>
2021-04-17 22:22:22 +0530

    first commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sagarkumarnayak/Downloads/school_learning_management-main/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
